6bdd22f992245f537a38ad97b41357bd0e6cbe05
reiss-josh <reiss.joshuae@gmail.com>
2020-08-20 18:19:28 -0700

    added controller aiming

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public class PlayerAim : MonoBehaviour
 {
     private Transform aimTransform;
+    bool mouseMovedLast = true;
     // Start is called before the first frame update
     void Awake()
     {
         aimTransform = GameObject.Find("Bow").transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = GetMousePosition();
-        var vect = mousePosition - transform.position;
+        UpdateAimAngle();
+    }
+
+    void UpdateAimAngle()
+    {
+        float mouseMoved = Input.GetAxis("Mouse Y") + Input.GetAxis("Mouse X");
+        float horizAim = Input.GetAxis("HorizontalAim");
+        float vertAim = Input.GetAxis("VerticalAim");
+
+        if (mouseMoved != 0) mouseMovedLast = true;
+        if (horizAim + vertAim != 0) mouseMovedLast = false;
+        Vector2 vect;
+        if (mouseMovedLast)
+        {
+            Vector3 mousePosition = GetMousePosition();
+            vect = mousePosition - transform.position;
+        }
+        else if (horizAim + vertAim != 0)
+        {
+            vect = new Vector2(horizAim, -vertAim);
+        }
+        else return;
         var ang = Vector2.SignedAngle(Vector2.right, vect);
-        aimTransform.rotation = Quaternion.Euler(0,0,ang);
+        aimTransform.rotation = Quaternion.Euler(0, 0, ang);
     }
 
     Vector3 GetMousePosition()
